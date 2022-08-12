177219c65408b452c9950c62c2211b224f4877d8
padoinedson <padoin.edson@gmail.com>
2022-08-12 20:03:01 -0300

    atualizando

---
+++ 2pthreadmutex/4soma_vetor_var_global_mutex.c
@@ -0,0 +1,92 @@
+// as th fazem as somas em uma var global com mutex
+
+#include <pthread.h>
+#include <stdio.h>
+#include "tempo.h"
+
+#define TAM 100000000
+
+//variaveis de escopo global
+int 	vetor[TAM];
+unsigned long	somaglobal;
+pthread_mutex_t m;
+
+void inicializa_vetor();
+void mostra_vetor();
+void *soma_vetor(void *arg);
+
+
+int main(int argc, char *argv[])
+{
+
+	tempo1();
+
+        pthread_t 	t1, t2;
+        int 		x, y;
+
+	inicializa_vetor();
+
+	//mostra_vetor();
+	
+	pthread_mutex_init(&m,NULL);
+	somaglobal = 0;
+
+	x = 0;	
+	pthread_create(&t1, NULL, soma_vetor, (void *)&x);
+
+	y = TAM / 2;	
+	pthread_create(&t2, NULL, soma_vetor, (void *)&y);
+
+
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+
+        printf("\n Soma total = %lu \n\n", somaglobal);
+
+	tempo2();
+	tempoFinal("mili segundos", argv[0], MSGLOG); 
+}
+
+
+
+void * soma_vetor(void *arg)
+{
+	int x, pos = *((int*)arg);
+
+	pthread_t pid = pthread_self();
+
+        printf("\n thread inicia na pos = %d ", pos);
+       // sleep(5);
+
+        for (x=pos; x < (pos+TAM/2); x ++){
+
+		pthread_mutex_lock(&m);
+                   somaglobal += vetor[x];
+		pthread_mutex_unlock(&m);
+
+		//printf("\nThread %lu - Soma = %d\n", pid, somaglobal);
+	}
+	pthread_exit(NULL);
+}
+
+void mostra_vetor()
+{
+        int z;
+        printf("\n  v e t o r \n");
+        for (z=0; z < TAM; z ++){
+                printf("\n Posicao %d - valor %d", z, vetor[z]);
+        }
+	printf("\n\n\n");
+}
+
+void inicializa_vetor()
+{
+        int z;
+        printf("\n inicializando vetor ...\n");
+        for (z=0; z < TAM; z ++)
+                vetor[z] = z;
+}
+
+
+
+
---
+++ 2pthreadmutex/5soma_vetor_var_local_mutex.c
@@ -0,0 +1,96 @@
+// as threads somam em uma var local para evitar concorrencia e atualiza na global
+
+#include <pthread.h>
+#include <stdio.h>
+#include "tempo.h"
+
+#define TAM 100000000
+
+//variaveis de escopo global
+int 	vetor[TAM],
+	somaglobal;
+pthread_mutex_t m;
+
+
+void inicializa_vetor();
+void mostra_vetor();
+void *soma_vetor(void *arg);
+
+
+int main(int argc, char *argv[])
+{
+
+	tempo1();
+
+        pthread_t 	t1, t2;
+        int 		x, y;
+
+	inicializa_vetor();
+
+	//mostra_vetor();
+	
+	pthread_mutex_init(&m,NULL);
+	somaglobal = 0;
+
+	x = 0;	
+	pthread_create(&t1, NULL, soma_vetor, (void *)&x);
+
+	y = TAM / 2;	
+	pthread_create(&t2, NULL, soma_vetor, (void *)&y);
+
+
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+
+        printf("\n Soma total = %d \n\n", somaglobal);
+
+	tempo2();
+	tempoFinal("mili segundos", argv[0], MSGLOG); 
+}
+
+
+
+void * soma_vetor(void *arg)
+{
+	int x, pos = *((int*)arg);
+	int somalocal = 0;
+
+	pthread_t pid = pthread_self();
+
+        printf("\n thread inicia na pos = %d ", pos);
+       // sleep(5);
+
+        for (x=pos; x < (pos+TAM/2); x ++)
+                somalocal += vetor[x];
+
+	pthread_mutex_lock(&m);
+	   somaglobal += somalocal;
+	pthread_mutex_unlock(&m);
+
+	printf("\nThread %lu - SomaGlobal = %d - SomaLocal =   %d \n", pid, somaglobal, somalocal);
+
+
+	pthread_exit(NULL);
+}
+
+void mostra_vetor()
+{
+        int z;
+        printf("\n  v e t o r \n");
+        for (z=0; z < TAM; z ++){
+                printf("\n Posicao %d - valor %d", z, vetor[z]);
+        }
+	printf("\n\n\n");
+}
+
+void inicializa_vetor()
+{
+        int z;
+        printf("\n inicializando vetor ...\n");
+        for (z=0; z < TAM; z ++)
+                vetor[z] = z;
+}
+
+
+
+
