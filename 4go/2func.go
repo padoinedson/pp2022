9cbb791cdefdbd20099ba154d4d909682d6f069a
padoinedson <padoin.edson@gmail.com>
2022-10-10 15:37:19 -0300

    update

---
+++ 4go/2func.go
@@ -0,0 +1,24 @@
+
+package main
+
+import ("fmt" 
+)
+
+
+func main() {
+
+	fmt.Printf("\n começando \n\n")
+
+	mostranumero(10)
+
+	fmt.Printf("\n terminando \n\n")
+}
+
+
+
+func mostranumero(n int) {
+
+
+	fmt.Printf(" numero %d\n", n)
+
+}
